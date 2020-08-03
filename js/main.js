5623864e3ab4ef8ed2c29d63b12ca7940eac65d5
sanjaysurendran <sanjaysurendran4539@gmail.com>
2020-08-03 13:25:18 +0530

    finale file

---
+++ js/main.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function(){
+
+	$(".text").typed({
+		strings:["<strong> Krayons</strong> <strong class='primary'>CreativeHub</strong>","<strong>Design Your Need </strong> <strong class='primary'>With Us</strong>"],
+		typespeed:0,
+		loop:true
+	
+	});
+	
+});
+jQuery(document).ready(function(){
+
+	$(".text1").typed({
+		strings:[" ഭംഗിയുള്ളത് ചിലത് അങ്ങനെയാണ് <strong class='primary'>കണ്ണിന്റെ തോന്നലുകൾ</strong>","<strong>krayons</strong> <strong class='primary'>CreativeHub</strong>"],
+		typespeed:0,
+		loop:true
+	
+	});
+	
+});
+
